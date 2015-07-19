01a40fceb00104f2211d8078b79b1724351aae21
Zander Martineau <zander@martineau.tv>
2015-07-19 20:36:46 +0100

    Use Ractive to populate dummy data

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
+var examples  = {
+	lorem: 'Suspendisse dictum feugiat nisl ut dapibus. Mauris iaculis porttitor posuere. Praesent id metus massa, ut blandit odio. Proin quis tortor orci. Etiam at risus et justo dignissim congue. Donec congue lacinia dui, a porttitor lectus condimentum laoreet. Nunc eu ullamcorper orci. Quisque eget odio ac lectus vestibulum faucibus eget in metus. In pellentesque faucibus vestibulum. Nulla at nulla justo, eget luctus tortor. Nulla facilisi. Duis aliquet egestas purus in blandit. Curabitur vulputate, ligula lacinia scelerisque tempor, lacus lacus ornare ante, ac egestas est urna sit amet arcu. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos.',
+	kafka: 'One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin. He lay on his armour-like back, and if he lifted his head a little he could see his brown belly, slightly domed and divided by arches into stiff sections. The bedding was hardly able to cover it and seemed ready to slide off any moment. His many legs, pitifully thin compared with the size of the rest of him, waved about helplessly as he looked.',
+	pangram: 'The quick, brown fox jumps over a lazy dog. DJs flock by when MTV ax quiz prog. Junk MTV quiz graced by fox whelps. Bawds jog, flick quartz, vex nymphs. Waltz, bad nymph, for quick jigs vex! Fox nymphs grab quick-jived waltz. Brick quiz whangs jumpy veldt fox. Bright vixens jump; dozy fowl quack. Quick wafting zephyrs vex bold Jim. Quick zephyrs blow, vexing daft Jim. Sex-charged fop blew my junk TV quiz. How quickly daft jumping zebras vex. Two driven jocks help fax my big quiz. Quick, Baz, get my woven flax jodhpurs!'
+};
+
 var clozeTest = new Ractive({
 	el: '#clozeTest',
 	template: '#template-clozeTest',
@@ -6,6 +12,18 @@ var clozeTest = new Ractive({
 		input: 'Add your own text here..'
 	}
 
+});
+
+clozeTest.on('eg-lorem', function() {
+	clozeTest.set('input', examples.lorem);
+});
+
+clozeTest.on('eg-kafka', function() {
+	clozeTest.set('input', examples.kafka);
+});
+
+clozeTest.on('eg-pangram', function() {
+	clozeTest.set('input', examples.pangram);
 });
 
 
@@ -28,11 +46,6 @@ var clozeTest = new Ractive({
 })(jQuery);
 
 $(function() {
-	$('#placeholders a').click(function() {
-		var thisText = $(this).attr('data-text');
-		$('textarea').text(thisText).keyup();
-		return false;
-	});
 	//Textarea move to result box
 	$('textarea').bind('keyup',function() {
 		$('#result_content').text($('textarea').val());
